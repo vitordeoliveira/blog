cdea1ac031e45aafff496083385a3c41cd192613
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-03-07 17:34:17 +0100

    filter posts on homepage to just show the finished ones

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -5,6 +5,9 @@ pub mod blog;
 use crate::{error::Result, model::Markdown, view::homepage};
 
 pub async fn home() -> Result<impl IntoResponse> {
-    let markdownlist = Markdown::list()?;
+    let markdownlist = Markdown::list()?
+        .into_iter()
+        .filter(|i| i.finished)
+        .collect();
     homepage(markdownlist)
 }
