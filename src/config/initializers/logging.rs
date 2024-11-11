c06f84ac30332ee5d5d8bcf607062a680bb496c0
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-11-11 11:37:19 +0000

    remove initializers folder idea

--- src/config/initializers/logging.rs
+++
@@ -1,8 +0,0 @@
-use crate::config::environment;
-
-pub fn execute() {
-    tracing_subscriber::fmt()
-        .with_env_filter(environment().rust_log.clone())
-        .with_thread_ids(true)
-        .init();
-}
