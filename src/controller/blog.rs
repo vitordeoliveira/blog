43ab8e533896068351044581cff40e66d38e11aa
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-11-11 11:37:53 +0000

    remove the idea of maintain the controler with the routes

--- src/controller/blog.rs
+++ src/controller/blog.rs
@@ -1,4 +1,4 @@
-use axum::{extract::Path, response::IntoResponse, routing::get, Router};
+use axum::{extract::Path, response::IntoResponse};
 use tracing::{info, warn};
 
 use crate::{
@@ -7,38 +7,24 @@ use crate::{
     view,
 };
 
-#[derive(Default)]
-pub struct Blog {
-    pub routes: axum::Router,
-    // pub routes_api: axum::Router,
-}
+pub async fn show(Path(postname): Path<String>) -> Result<impl IntoResponse> {
+    info!("show");
+    Markdown::add_views_to_markdown(&postname).await?;
+    let markdown = Markdown::new(postname)?;
 
-impl Blog {
-    pub fn new() -> Self {
-        Self {
-            routes: Router::new().route("/:id", get(Self::show)),
-        }
-    }
+    let similar_posts_metadata = markdown.metadata.similar_posts.iter().map(|i| async move {
+        Markdown::list_markdown_info_of_post(format!("{}.md", i))
+            .await
+            .map_err(|e| warn!("{e}"))
+    });
 
-    async fn show(Path(postname): Path<String>) -> Result<impl IntoResponse> {
-        info!("show");
-        Markdown::add_views_to_markdown(&postname).await?;
-        let markdown = Markdown::new(postname)?;
+    let similar_posts_metadata: Vec<(MarkdownMetadata, PostInfo)> =
+        futures::future::join_all(similar_posts_metadata)
+            .await
+            .into_iter()
+            .flatten()
+            // .filter_map(|e| e.ok())
+            .collect();
 
-        let similar_posts_metadata = markdown.metadata.similar_posts.iter().map(|i| async move {
-            Markdown::list_markdown_info_of_post(format!("{}.md", i))
-                .await
-                .map_err(|e| warn!("{e}"))
-        });
-
-        let similar_posts_metadata: Vec<(MarkdownMetadata, PostInfo)> =
-            futures::future::join_all(similar_posts_metadata)
-                .await
-                .into_iter()
-                .flatten()
-                // .filter_map(|e| e.ok())
-                .collect();
-
-        Ok(view::blog::show(markdown, similar_posts_metadata))
-    }
+    Ok(view::blog::show(markdown, similar_posts_metadata))
 }
