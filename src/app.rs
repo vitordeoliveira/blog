5333e518e2269ccad0bcc3a10477e634ae797c86
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-11-11 14:24:25 +0000

    update app.rs to pass sqlite path

--- src/app.rs
+++ src/app.rs
@@ -8,9 +8,10 @@ use crate::{
 };
 
 pub async fn new_app(_db_connection: &str, assets_path: &str) -> Result<axum::Router> {
-    let app_state = AppState::new()?;
+    let app_state = AppState::new("blog.sqlite")?;
 
     let blog_routes = Router::new().route("/:id", get(show));
+
     let router = Router::new() // `GET /` goes to `root`
         .route("/", get(home))
         .nest("/blog", blog_routes)
