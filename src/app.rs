a63131b6d318e4e4b74936281ec6d455da32330f
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-11-11 11:38:17 +0000

    create app.rs
    useful for testing

---
+++ src/app.rs
@@ -0,0 +1,23 @@
+use anyhow::{Ok, Result};
+use axum::{routing::get, Router};
+use tower_http::services::{ServeDir, ServeFile};
+
+use crate::{
+    controller::{blog::show, home},
+    AppState,
+};
+
+pub async fn new_app(_db_connection: &str, assets_path: &str) -> Result<axum::Router> {
+    let app_state = AppState::new()?;
+
+    let blog_routes = Router::new().route("/:id", get(show));
+    let router = Router::new() // `GET /` goes to `root`
+        .route("/", get(home))
+        .nest("/blog", blog_routes)
+        // .nest("/api/v1/blog", self.routes.blog_api)
+        .with_state(app_state)
+        .route_service("/sitemap.xml", ServeFile::new("sitemap.xml"))
+        .nest_service("/assets", ServeDir::new(format!("{}/assets", assets_path)));
+
+    Ok(router)
+}
