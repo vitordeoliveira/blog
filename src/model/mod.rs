2b6c0df8abbc283c233d132f7d13a2729547a38b
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-11-12 16:40:21 +0000

    database migration start (WIP)

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,13 +1,12 @@
 use std::fs;
 
+use anyhow::Result;
 use pulldown_cmark::Options;
+use rusqlite::{params, Connection};
 use serde::{Deserialize, Serialize};
 use yaml_front_matter::YamlFrontMatter;
 
-use crate::{
-    config::database::firestore,
-    error::{Error, Result},
-};
+use crate::error::ServerError;
 
 pub struct Markdown {
     pub metadata: MarkdownMetadata,
@@ -20,13 +19,72 @@ pub struct PostInfo {
     pub views: u16,
 }
 
-const COLLECTION_NAME: &str = "posts";
+#[derive(Debug)]
+struct Post {
+    // TODO: maybe remove this id field, for now is really necessary
+    id: u16,
+    title: String,
+    views: u16,
+}
+
+trait SqliteOperations {
+    fn find_or_create_post(sqlite_conn: &Connection, title: &str) -> Result<PostInfo, ServerError>;
+    fn increment_views(sqlite_conn: &Connection, title: &str) -> Result<(), ServerError>;
+}
+
+impl SqliteOperations for Markdown {
+    fn find_or_create_post(sqlite_conn: &Connection, title: &str) -> Result<PostInfo, ServerError> {
+        let mut stmt =
+            sqlite_conn.prepare_cached("SELECT id, title, views FROM posts WHERE title = ?1")?;
+
+        let post = stmt.query_row([title], |row| {
+            Ok(Post {
+                id: row.get(0)?,
+                title: row.get(1)?,
+                views: row.get(2)?,
+            })
+        });
+
+        Ok(match post {
+            Ok(post) => PostInfo {
+                id: post.title,
+                views: post.views,
+            },
+            Err(rusqlite::Error::QueryReturnedNoRows) => {
+                sqlite_conn.execute("INSERT INTO posts (title) values (?1)", params![title])?;
+
+                let post = stmt.query_row([title], |row| {
+                    Ok(Post {
+                        id: row.get(0)?,
+                        title: row.get(1)?,
+                        views: row.get(2)?,
+                    })
+                })?;
+
+                PostInfo {
+                    id: post.title,
+                    views: post.views,
+                }
+            }
+            Err(e) => return Err(ServerError::DBError(e)),
+        })
+    }
+
+    fn increment_views(sqlite_conn: &Connection, title: &str) -> Result<(), ServerError> {
+        sqlite_conn.execute(
+            "UPDATE posts SET views = views + 1 WHERE title = ?1",
+            params![title],
+        )?;
+
+        Ok(())
+    }
+}
 
 impl Markdown {
     pub fn new(postname: String) -> Result<Self> {
         let file = format!("./blogpost/{}.md", &postname);
         let markdown_file =
-            fs::read_to_string(file).map_err(|_| Error::PageNotFound(postname.clone()))?;
+            fs::read_to_string(file).map_err(|_| ServerError::PageNotFound(postname.clone()))?;
         let metadata = MarkdownMetadata::new(&markdown_file)?;
         let content_raw = MarkdownMetadata::extract(&markdown_file)?;
         let mut content = String::new();
@@ -44,57 +102,9 @@ impl Markdown {
         Ok(Self { metadata, content })
     }
 
-    async fn get_views_or_create(title: &str) -> Result<PostInfo> {
-        let db = firestore().await;
-
-        let post = db
-            .fluent()
-            .select()
-            .by_id_in(COLLECTION_NAME)
-            .obj()
-            .one(title)
-            .await
-            .map_err(|_| Error::InternalServer("Error on query".to_string()))?;
-
-        Ok(match post {
-            Some(data) => data,
-            None => {
-                db.fluent()
-                    .insert()
-                    .into(COLLECTION_NAME)
-                    .document_id(title)
-                    .object(&PostInfo {
-                        id: title.to_string(),
-                        views: 0,
-                    })
-                    .execute::<PostInfo>()
-                    .await
-                    .unwrap();
-
-                PostInfo {
-                    id: title.to_string(),
-                    views: 0,
-                }
-            }
-        })
-    }
-
-    pub async fn add_views_to_markdown(title: &str) -> Result<()> {
-        let db = firestore().await;
-        let postview = Markdown::get_views_or_create(title).await?;
-
-        db.fluent()
-            .update()
-            .in_col(COLLECTION_NAME)
-            .document_id(title)
-            .object(&PostInfo {
-                views: postview.views + 1,
-                ..postview
-            })
-            .execute::<PostInfo>()
-            .await
-            .unwrap();
-        Ok(())
+    pub async fn add_views_to_markdown(title: &str) -> Result<(), ServerError> {
+        let sqlite_conn = Connection::open("./data/blog.sqlite")?;
+        Self::increment_views(&sqlite_conn, title)
     }
 
     pub async fn list_markdown_info() -> Result<Vec<(MarkdownMetadata, PostInfo)>> {
@@ -105,9 +115,12 @@ impl Markdown {
         for path in paths {
             let filepath = path.unwrap().path().display().to_string();
             let markdown_file = fs::read_to_string(&filepath)
-                .map_err(|_| Error::PageNotFound(filepath.to_string()))?;
+                .map_err(|_| ServerError::PageNotFound(filepath.to_string()))?;
             let metadata = MarkdownMetadata::new(&markdown_file)?;
-            let post: PostInfo = Self::get_views_or_create(&metadata.filename).await?;
+            // let post: PostInfo = Self::get_views_or_create(&metadata.filename).await?;
+
+            let sqlite_conn = Connection::open("./data/blog.sqlite")?;
+            let post: PostInfo = Self::find_or_create_post(&sqlite_conn, &metadata.filename)?;
 
             markdown_info.push((metadata, post));
         }
@@ -119,10 +132,11 @@ impl Markdown {
         filepath: String,
     ) -> Result<(MarkdownMetadata, PostInfo)> {
         let markdown_file = fs::read_to_string(format!("./blogpost/{}", &filepath))
-            .map_err(|_| Error::PageNotFound(filepath.to_string()))?;
+            .map_err(|_| ServerError::PageNotFound(filepath.to_string()))?;
         let metadata = MarkdownMetadata::new(&markdown_file)?;
-        let post: PostInfo = Self::get_views_or_create(&metadata.filename).await?;
-
+        // TODO: ok, I am creating connections all the time, use the app state insead
+        let sqlite_conn = Connection::open("./data/blog.sqlite")?;
+        let post: PostInfo = Self::find_or_create_post(&sqlite_conn, &metadata.filename)?;
         Ok((metadata, post))
     }
 }
@@ -143,14 +157,14 @@ pub struct MarkdownMetadata {
 impl MarkdownMetadata {
     fn new(input: &str) -> Result<Self> {
         let result = YamlFrontMatter::parse::<MarkdownMetadata>(input)
-            .map_err(|_| Error::InternalServer("Error on YamlFrontMatter".to_string()))?;
+            .map_err(|_| ServerError::InternalServer("Error on YamlFrontMatter".to_string()))?;
 
         Ok(result.metadata)
     }
 
     fn extract(string_output: &str) -> Result<String> {
         let regex = regex::Regex::new(r"---((.|\n)*?)---")
-            .map_err(|err| Error::InternalServer(err.to_string()))?;
+            .map_err(|err| ServerError::InternalServer(err.to_string()))?;
 
         Ok(regex.replace(string_output, "").to_string())
     }
