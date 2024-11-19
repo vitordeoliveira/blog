62f447a619d7b73a40b7a62a8832fab18c0c13c7
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-11-19 07:11:49 +0000

    update model folder

---
+++ src/model/sqlite.rs
@@ -0,0 +1,15 @@
+use crate::error::ServerError;
+use anyhow::Result;
+use rusqlite::Connection;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct PostInfo {
+    pub id: String,
+    pub views: u16,
+}
+
+pub trait SqliteOperations {
+    fn find_or_create_post(sqlite_conn: &Connection, title: &str) -> Result<PostInfo, ServerError>;
+    fn increment_views(sqlite_conn: &Connection, title: &str) -> Result<(), ServerError>;
+}
