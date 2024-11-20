48bf6455a44c4720ce9542897d18eac66c420789
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-11-20 09:46:21 +0000

    move code from mod to home

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -1,37 +1,4 @@
-use anyhow::Result;
-use askama::Template;
-use axum::{
-    http::StatusCode,
-    response::{Html, IntoResponse},
-};
-
-use crate::{
-    error::{self, ServerError},
-    model::{MarkdownMetadata, PostInfo},
-};
-
 pub mod blog;
 pub mod home;
 
-#[derive(Template)]
-#[template(path = "root.html")]
-struct RootTemplate {
-    title: String,
-    posts: Vec<(MarkdownMetadata, PostInfo)>,
-}
-
-pub fn homepage(
-    posts: Vec<(MarkdownMetadata, PostInfo)>,
-) -> Result<impl IntoResponse, ServerError> {
-    let root = RootTemplate {
-        title: "vitor.ws".to_string(),
-        posts,
-    };
-
-    let html = match root.render() {
-        Ok(html) => html,
-        Err(err) => return Err(error::ServerError::InternalServer(err.to_string())),
-    };
-
-    Ok((StatusCode::OK, Html(html)))
-}
+pub use home::homepage;
