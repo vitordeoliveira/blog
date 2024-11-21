8698004c3504bbbb7a52bb9d30250a24f2daa3be
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-11-21 20:34:58 +0000

    update controller blog.rs

--- src/controller/blog.rs
+++ src/controller/blog.rs
@@ -1,19 +1,21 @@
 use anyhow::Result;
 use axum::{
+    body::Body,
     extract::{Path, State},
-    http::{header, Response},
+    http::{header, Request, Response},
     response::IntoResponse,
-    Json,
+    Extension, Json,
 };
 use rusqlite::Connection;
 use tracing::{instrument, warn};
 
 use crate::{
     error::ServerError,
-    model::{Markdown, MarkdownMetadata, PostInfo},
+    model::{Markdown, MarkdownMetadata, PostInfo, User},
     view, AppState,
 };
 
+#[instrument]
 pub async fn load_markdown_content_api(
     State(state): State<AppState>,
     Path(postname): Path<String>,
@@ -27,6 +29,24 @@ pub async fn load_markdown_content_api(
         .unwrap())
 }
 
+#[instrument]
+pub async fn markdown_list_api(
+    State(state): State<AppState>,
+    Extension(user): Extension<User>,
+) -> Result<impl IntoResponse, ServerError> {
+    let sqlite_conn = state.get_connection()?;
+
+    let markdownlist: Vec<(MarkdownMetadata, PostInfo)> =
+        Markdown::list_private_markdown_info(sqlite_conn, user.id)
+            .await?
+            .into_iter()
+            .filter(|i| i.0.finished)
+            .collect();
+
+    Ok(Json(markdownlist))
+}
+
+#[instrument]
 pub async fn load_markdown_similar_posts_api(
     State(state): State<AppState>,
     Path(postname): Path<String>,
@@ -148,7 +168,8 @@ mod tests {
                     similar_posts: vec!["test-markdown".to_string()],
                     date: "2024-04-03t17:52:00".to_string(),
                     finished: false,
-                    image_preview: None
+                    image_preview: None,
+                    owner: None
                 },
                 PostInfo {
                     id: "test-markdown-2".to_string(),
