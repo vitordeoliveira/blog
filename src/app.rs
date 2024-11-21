6d6cfb85c5cd8a27860774cd11fab2a50c7c4aee
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-11-21 20:34:16 +0000

    update app.rs to have api_key auth

--- src/app.rs
+++ src/app.rs
@@ -1,24 +1,64 @@
-use anyhow::{Ok, Result};
-use axum::{routing::get, Router};
+use std::str::FromStr;
+
+use anyhow::Result;
+use axum::{
+    extract::{Request, State},
+    middleware::{self, Next},
+    response::Response,
+    routing::get,
+    Router,
+};
+
 use tower_http::services::{ServeDir, ServeFile};
+use tracing::instrument;
+use uuid::Uuid;
 
 use crate::{
     controller::{
-        blog::{load_markdown_content_api, load_markdown_similar_posts_api, show},
+        blog::{
+            load_markdown_content_api, load_markdown_similar_posts_api, markdown_list_api, show,
+        },
         home,
     },
-    AppState,
+    error::ServerError,
+    model::Markdown,
+    AppState, SqliteOperations,
 };
+
+#[instrument]
+async fn api_key_auth(
+    state: State<AppState>,
+    mut request: Request,
+    next: Next,
+) -> Result<Response, ServerError> {
+    let api_key = request.headers().get("api_key");
+    if let Some(api_key) = api_key {
+        let sqlite_conn = state.get_connection()?;
+
+        let api_key = Uuid::from_str(api_key.to_str().unwrap())?;
+        let user = Markdown::get_user_from_api_key(&sqlite_conn, &api_key)?;
+
+        request.extensions_mut().insert(user);
+        Ok(next.run(request).await)
+    } else {
+        Err(ServerError::Unauthorized)
+    }
+}
 
 pub async fn new_app(app_state: AppState, assets_path: &str) -> Result<axum::Router> {
     let blog_view = Router::new().route("/:id", get(show));
 
     let blog_api = Router::new()
+        .route("/v1/blog", get(markdown_list_api))
         .route("/v1/blog/:id", get(load_markdown_content_api))
         .route(
             "/v1/blog/:id/similar-posts",
             get(load_markdown_similar_posts_api),
-        );
+        )
+        .layer(middleware::from_fn_with_state(
+            app_state.clone(),
+            api_key_auth,
+        ));
 
     let api = Router::new().nest("/api", blog_api);
 
@@ -26,7 +66,6 @@ pub async fn new_app(app_state: AppState, assets_path: &str) -> Result<axum::Rou
         .route("/", get(home))
         .nest("/blog", blog_view)
         .merge(api)
-        // .nest("/api/v1/blog", self.routes.blog_api)
         .with_state(app_state)
         .route_service("/sitemap.xml", ServeFile::new("sitemap.xml"))
         .nest_service("/assets", ServeDir::new(format!("{}/assets", assets_path)));
