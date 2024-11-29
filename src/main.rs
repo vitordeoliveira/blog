c24c6a4161f0bf8e4f57d3701becd41293c289b1
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-11-29 14:42:30 +0000

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Result};
-use blog::{app, config, error::ServerError, AppState};
+use blog::{app, config, error::ServerError, AppState, ConfigState, EnvState};
 use std::env;
 
 #[tokio::main]
@@ -8,6 +8,7 @@ async fn main() -> Result<(), ServerError> {
     let port = env!("SERVER_PORT");
     let rust_log = env!("RUST_LOG");
     let assets_path = env!("CARGO_MANIFEST_DIR");
+    let app_key = env!("APP_KEY");
 
     let sqlite_db_path = env::var("SQLITE_DB_PATH")
         .context("SQLITE_DB must be defined")
@@ -31,9 +32,14 @@ async fn main() -> Result<(), ServerError> {
         .context("Failed to start tokio listener")
         .unwrap();
 
-    let app_state: AppState = AppState::new(&sqlite_db_path, &rust_env)?;
+    let config_state: ConfigState = ConfigState::new(&blog_config_path, assets_path, app_key);
+    let env_state: EnvState = EnvState::new(&sqlite_db_path, &rust_env)?;
+    let state = AppState {
+        config_state,
+        env_state,
+    };
 
-    let app = app::new_app(app_state, assets_path).await?;
+    let app = app::new_app(state).await?;
 
     tracing::info!("router initialized, now listening on port {}", port);
 
